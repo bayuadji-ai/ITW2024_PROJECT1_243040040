1b7c00b9f7be5396bf72d1325914e74f44767d72
Bayu Adji Prakoso <bayuadjiprakoso@gmail,com>
2024-12-20 19:48:21 +0700

    finishing

--- js/script.js
+++ js/script.js
@@ -10,3 +10,28 @@ menuIcon.onclick = () => {
 /*===remove toggle icon navbar===*/
 menuIcon.classList.remove("bx-x");
 navbar.classList.remove("active");
+
+/*===remove toggle icon navbar===*/
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .skills-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+/*===text flow===*/
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Web Design", "Mahasiswa"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
